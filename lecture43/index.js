064ba6c1eee6a6ee80ea8ef0f1858f02f4b3fd93
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-07-26 19:21:20 +0530

    js function

---
+++ lecture43/index.js
@@ -0,0 +1,81 @@
+// Function
+// => Function is a block of code perform a specific task.
+
+// function define 
+// function myName(){
+//     console.log("Ajay Kumar");
+    
+// }
+
+// // function use - function call
+// myName();
+
+// function printCount(){
+//     for(let i =1; i<=10; i++){
+//         console.log(i);
+        
+//     }
+// }
+
+// printCount();
+
+
+// function printNumber(num){
+// console.log("print number :",num);
+
+// }
+// printNumber(10);
+
+// function getAverage(num1, num2){
+//  let avg = (num1 + num2)/2;
+//  console.log(avg);
+ 
+// }
+
+// getAverage(12,15);
+
+
+// Return Function
+
+// function getSum(a,b,c){
+// let sum = a + b + c;
+// return sum;
+// }
+
+// // console.log(getSum(1,4,5));
+//  let ans = getSum(1,5,8);
+//  console.log(ans);
+ 
+
+// function getmyName(firstName , lastName) {
+//     let fullName = firstName + " " +lastName
+//     return fullName;
+
+//     // unreachable statements
+//     let a = 10;
+//     let b = 15;
+//     let sum = a + b;
+//     console.log(sum);
+// }
+
+// let fullName = getmyName("Ajay", "Kumar")
+// console.log(fullName);
+
+let squareNumber = function(num){
+    let ans = num**2;
+    return ans;
+}
+
+let ans = squareNumber(4);
+console.log(ans);
+console.log(squareNumber(25));
+
+
+// Arrow Function
+let getExp = (x,y) => {
+    let ans = x**y;
+    return ans;
+}
+
+console.log(getExp(2,5));
+
