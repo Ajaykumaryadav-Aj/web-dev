c31dd7d4e722d583d4e2243b93582ea525994c9a
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-07-30 01:31:45 +0530

    variable scope

---
+++ l-46-TDZ/index.js
@@ -0,0 +1,33 @@
+// Global Scope
+// let global = 6;  // Global Scope , it can be accessed anywhere in our code
+
+// function greet() {
+//   console.log(global);
+    
+// }
+// console.log(global);
+// greet();
+
+
+// function scope
+
+// function test() {
+//     var name = "Ajay";
+//     console.log(name);
+    
+// }
+// test();
+// console.log(name);  // Error: name is not defined , because var not accessible outside the function
+
+
+
+// Block Scope
+// {
+//     let x = 5;
+//     console.log(x);
+// }
+// console.log(x);   // Error : x is not defined . it is follow block scope
+
+console.log(marks);
+
+let marks = 56;
