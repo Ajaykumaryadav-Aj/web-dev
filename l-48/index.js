2b2c9116dd68db2b970d090f78886deb1d8fc4ec
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-07-31 23:56:16 +0530

    math

---
+++ l-48/index.js
@@ -0,0 +1,45 @@
+let curr = new Date();
+
+let date = new Date('June 20 2002 7:15')
+
+let newDate = new Date(2002, 4 ,1, 4)
+
+console.log(newDate);
+
+console.log(newDate.setFullYear(2000));
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(12,14,5,77,88,368,4));
+
+// console.log(Math.min(1,5,6,3,2,47));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.ceil(1.2));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(2));
+
+// console.log(Math.pow(2,5));
+
+
+
+
+
+
