234fe715b30a52a6acdf17fbb944cf8c8b7a4924
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-08-01 23:57:05 +0530

    object cloning and error handling

---
+++ l-50-ErrorHandling/index.js
@@ -0,0 +1,37 @@
+// compile time Error
+// console.log("B";    // syntax Error
+
+
+// Runtime Error
+// console.log(x);    // Reference Error
+
+
+// try {
+//     console.log("try block is start here");
+    
+//     console.log(x);   // error=> Reference Error
+
+//     console.log("try block is end here");
+    
+    
+// } catch (error) {
+//     console.log("I am catch block");
+//     console.log(error);
+    
+    
+// }finally{
+//     console.log("I am finally block run everytime");
+    
+// }
+
+
+
+// lets creat custom error
+
+try{
+    console.log(x);
+}
+catch(err){
+throw new Error("pahle x ko declare to ker , phir print krna");
+
+}
