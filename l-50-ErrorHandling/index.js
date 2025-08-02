b9f9a7b93cbd4f28b7adc603646e255373354b04
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-08-02 23:54:15 +0530

    Dom

--- l-50-ErrorHandling/index.js
+++ l-50-ErrorHandling/index.js
@@ -6,32 +6,38 @@
 // console.log(x);    // Reference Error
 
 
-// try {
-//     console.log("try block is start here");
-    
-//     console.log(x);   // error=> Reference Error
+// let x = 2;
+//  x.toUpperCase();  // typeError 
+//  console.log(x);
+ 
 
-//     console.log("try block is end here");
+try {
+    console.log("try block is start here");
+    let x = 10;
+   ///
+    console.log(x);   // error=> Reference Error
+
+    console.log("try block is end here");
     
     
-// } catch (error) {
-//     console.log("I am catch block");
-//     console.log(error);
+} catch (error) {
+    console.log("I am catch block");
+    console.log(error);
     
     
-// }finally{
-//     console.log("I am finally block run everytime");
+}finally{
+    console.log("I am finally block run everytime");
     
-// }
+}
 
 
 
 // lets creat custom error
 
-try{
-    console.log(x);
-}
-catch(err){
-throw new Error("pahle x ko declare to ker , phir print krna");
+// try{
+//     console.log(x);
+// }
+// catch(err){
+// throw new Error("pahle x ko declare to ker , phir print krna");
 
-}
+// }
